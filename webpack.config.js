a7eaf8d718e8e88f08765098eb74b7b969b30ab7
budnik9 <kirill.bubnik.95@gmail.com>
2018-07-09 02:33:49 +0300

    fixed errors and created header

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,101 @@
 const NODE_ENV = process.env.NODE_ENV || "development";
 const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const extractLess = new ExtractTextPlugin({
-	filename: "[name].[contenthash].css",
-	disable: NODE_ENV === "development"
+const extractLess = new MiniCssExtractPlugin({
+    filename: "[name].[hash].css",
+    disable: NODE_ENV === "development"
 });
 
 module.exports = {
-	mode: NODE_ENV,
-	entry: "./app.js",
-	output: {
-		path: __dirname + "/dist",
-		filename: "bundle.js"
-	},
+    mode: NODE_ENV,
+    entry: "./src/app.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
 
-	devtool: NODE_ENV == "development" ? "cheap-inline-module-source-map" : "source-map",
+    devtool: NODE_ENV == "development" ? "cheap-inline-module-source-map" : "source-map",
+	
+    plugins: [extractLess],
 
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: [
-							["env", {
-								targets: {
-									browsers: "> 5%"     // adaptation for browsers with a share of more than 5%
-								}
-							}]
-						]
-					}
-				}
-			},
-			// {
-			//     test: /\.css$/,
-			//     use: [ "style-loader", "css-loader", "postcss-loader" ]
-			// },
-			{
-				test: /\.less$/,
-				use: extractLess.extract({
-					use: [{
-						loader: "css-loader"
-					}, {
-						loader: "postcss-loader"
-					}, {
-						loader: "less-loader"
-					}],
-					// use style-loader in development
-					fallback: "style-loader"
-				})
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							name: "[path][name].[ext]",
-							outputPath: "images/"
-						}
-					}
-				]
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: [
-					{
-						loader: "url-loader",
-						options: {
-							limit: 8192
-						}
-					}
-				]
-			}
-		]
-	},
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["env", {
+                                targets: {
+                                    browsers: "> 5%"     // adaptation for browsers with a share of more than 5%
+                                }
+                            }],
+                            ["stage-0"]
+                        ]
+                    }
+                }
+            },
+            // {
+            //     test: /\.css$/,
+            //     use: [ "style-loader", "css-loader", "postcss-loader" ]
+            // },
+            {
+                test: /\.(c|le)ss$/,
+                use: [
+                    NODE_ENV === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "less-loader",
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[path][name].[ext]",
+                            outputPath: "images/"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+        ]
+    },
 
-	resolve: {                                       // rules for loading modules
-		modules: ["node_modules"],
-		extensions: [".js"]
-	},
+    resolve: {                                       // rules for loading modules
+        modules: ["node_modules"],
+        extensions: [".js"]
+    },
 
-	resolveLoader: {                                // rules for loading loaders
-		modules: ["node_modules"],
-		extensions: [".js"]
-	}
+    resolveLoader: {                                // rules for loading loaders
+        modules: ["node_modules"],
+        extensions: [".js"]
+    }
 };
 
 
 if(NODE_ENV == "production") {
-	module.exports.plugins.push(
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-				drop_console: true
-			}
-		})
-	);
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true
+            }
+        })
+    );
 }
